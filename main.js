05bc06e8d674a9d239d4944b270286b420b461c9
Le Tan Hiep <tanhiep24135@gmail.com>
2025-09-30 21:41:35 +0700

    Edit to running on port 3000

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Back4App, Node.js is running!");
+  res.send("23120255_23120262_23120264 Hello Back4App!");
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
